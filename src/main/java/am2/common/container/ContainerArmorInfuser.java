fc05af642f4552e34c6f623a0b18ce99f02ead7b
Menyhárt Róbert <xfrgod72@gmail.com>
2018-04-16 16:21:39 +0200

    Fixed invisible typo

--- src/main/java/am2/common/container/ContainerArmorInfuser.java
+++ src/main/java/am2/common/container/ContainerArmorInfuser.java
@@ -28,7 +28,7 @@ public class ContainerArmorInfuser extends Container{
 
 	@Override
 	public boolean canInteractWith(EntityPlayer entityplayer){
-		return tileEntity.isUseableByPlayer(entityplayer);
+		return tileEntity.isUsableByPlayer(entityplayer);
 	}
 
 	@Override
