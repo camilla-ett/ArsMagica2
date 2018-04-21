4fe2d054d02ad00722feae373553fc2dc325d200
sqbika <sqbika@gmail.com>
2018-04-21 18:08:52 +0200

    Reimplemented JEI AGAIN

--- src/main/java/am2/client/compat/JEIHandler.java
+++ src/main/java/am2/client/compat/JEIHandler.java
@@ -1,26 +1,26 @@
 package am2.client.compat;
 
+import am2.api.recipes.RecipeArsMagica;
 import am2.api.recipes.RecipesEssenceRefiner;
 import am2.client.compat.jei.EssenceRefinerRecipeCategory;
 import am2.client.compat.jei.EssenceRefinerRecipeHandler;
+import am2.client.compat.jei.EssenceRefinerRecipeWrapper;
 import mezz.jei.api.IJeiRuntime;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.recipe.IRecipeCategoryRegistration;
 
 @JEIPlugin
 public class JEIHandler implements IModPlugin{
 
 	@Override
 	public void register(IModRegistry registry) {
-		registry.addRecipeCategories(new EssenceRefinerRecipeCategory(registry.getJeiHelpers().getGuiHelper()));
-		registry.addRecipeHandlers(new EssenceRefinerRecipeHandler());
-		registry.addRecipes(RecipesEssenceRefiner.essenceRefinement().getAllRecipes());
+		registry.handleRecipes(RecipeArsMagica.class, EssenceRefinerRecipeWrapper::new, EssenceRefinerRecipeCategory.ID);
 	}
 
 	@Override
-	public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
-		
+	public void registerCategories(IRecipeCategoryRegistration registry) {
+		registry.addRecipeCategories(new EssenceRefinerRecipeCategory(registry.getJeiHelpers().getGuiHelper()));
 	}
-
 }
